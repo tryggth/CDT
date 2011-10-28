25fd527ce5948ad2a073700f7e7edaa0a50dc4b7
Adam Getchell <acgetchell@ucdavis.edu>
2011-10-28 04:07:47 -0700

    Deleted F#

--- FSharpCDT/FSharpCDT/Program.fs
+++
@@ -1,8 +0,0 @@
-﻿// Learn more about F# at http://fsharp.net
-
-open System
-
-// String array of arguments passed in the command line
-let args = Environment.GetCommandLineArgs()
-
-printfn "%A" args
