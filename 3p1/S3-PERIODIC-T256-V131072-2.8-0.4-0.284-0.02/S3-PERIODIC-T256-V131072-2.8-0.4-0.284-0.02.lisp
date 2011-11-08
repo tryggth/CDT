52d72b50a7933358e128f108a36683f62ccfecb1
Adam Getchell <acgetchell@ucdavis.edu>
2011-11-07 17:54:21 -0800

    Added directories from 2p1-safe and 3p1-safe

---
+++ 3p1/S3-PERIODIC-T256-V131072-2.8-0.4-0.284-0.02/S3-PERIODIC-T256-V131072-2.8-0.4-0.284-0.02.lisp
@@ -0,0 +1,23 @@
+(declaim (optimize (safety 0) (speed 3)))
+
+(load "../../utilities.lisp")
+(load "../globals.lisp")
+(load "../simplex.lisp")
+(load "../moves.lisp")
+(load "../initialization.lisp")
+(load "../montecarlo.lisp")
+
+(initialize-t-slices-with-v-volume :num-time-slices 256
+				   :target-volume (* 128 1024)
+				   :boundary-conditions "periodic")
+(set-kappa0-delta-kappa4 2.8 0.4 0.284)
+
+(setf NUM-SWEEPS 100000)
+(setf SAVE-EVERY-N-SWEEPS 1000)
+(format t "starting run ~A~%" (generate-filename))
+(generate-spacetime-and-movie-data)
+(format t "finished run ~A~%~%" (generate-filename))
+(setf NUM-SWEEPS 400000)
+(format t "starting run ~A~%" (generate-filename 100001))
+(generate-data-v3 100001)
+(format t "finished run ~A~%~%" (generate-filename 100001))
