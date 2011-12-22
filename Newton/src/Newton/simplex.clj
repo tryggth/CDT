ec321926ae15c9298c4614c713df1e05d62fc571
Adam Getchell <acgetchell@ucdavis.edu>
2011-12-21 23:53:47 -0800

    Building up simplex
    Initial test skeleton

--- Newton/src/Newton/simplex.clj
+++ Newton/src/Newton/simplex.clj
@@ -1,4 +1,5 @@
-(ns Newton.simplex)
+(ns Newton.simplex
+  (:use midje.sweet))
 
 ;; # Load Optional Resources
 ;; Use external Javascript and CSS in your documentation. For example:
@@ -15,6 +16,31 @@
 
 ;; A simplex is a d-dimensional triangle. It has d+1 points and
 ;; $\left(\begin{array}{c}d+1\\2\end{array}\right)$ edges
+;; A face is denoted by 3 edges; each edge has a point in common with another edge,
+;; and the edges should show cyclic permutation. Thus, for a set of 3 edges, there should only
+;; be 3 distinct points.
+;; Two simplices share a common face. They are then said to be neighbors.
+
+
+(unfinished type-id)
+
+(defn simplex?
+  "Tests that a simplex satisfies the following criteria:
+    every vertex has an edge with every other vertex
+    at least one vertex is on t-lo
+    at least one vertex is on t-hi
+    all faces are defined"
+  [& {type-id :type t-lo :time-lo t-hi :time-high points :vertices}]
 
 
 
+  )
+
+(fact
+  (simplex? :type type-id
+    :time-lo 0
+    :time-high 1
+    :vertices [0 1 2 3 4]) => truthy)
+
+;;"failing scenario too many or too few points"
+;;"abstract to d-simplex?"
