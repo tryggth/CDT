1086f64d7178d86efc762b0a5a13c81ab9ee25a7
Yurlungur <DrLoco3000@gmail.com>
2012-07-22 22:14:45 -0600

    modified:   de_sitter_with_boundaries.lisp 	new file:   map_phase_space_parallelized.py 	new file:   map_phase_space_parallelized.py~ 	modified:   parameter_tuning.lisp 	new file:   tuning_utilities.lisp 	new file:   tuning_utilities.lisp~

--- 2p1-fixed-boundaries/de_sitter_with_boundaries.lisp
+++ 2p1-fixed-boundaries/de_sitter_with_boundaries.lisp
@@ -16,19 +16,15 @@
 (setf SAVE-EVERY-N-SWEEPS 500)
 
 ;; Initialize a small spacetime for exploratory calculations
-(initialize-t-slices-with-v-volume :num-time-slices          28
-				   :target-volume            30850
-				   :spatial-topology         "s2"
-				   :boundary-conditions      "open"
-                                   :initial-spatial-geometry "TS2-V64-k1.0-kkk0.7577200487786184d0-bottom.boundary"
-				   :final-spatial-geometry   "TS2-V64-k1.0-kkk0.7577200487786184d0-top.boundary")
+(with-open-file (infile "S2-OPEN-T028-V030853-1.0-0.75772-0.02--1-000000001-000033000-000050000-on-bloch-start2012-07-19-16-46-34-curr2012-07-20-19-43-56.3sx2p1")
+  (load-spacetime-from-file infile))
 
 ;; Initialize the coupling constants to a set that should get us
 ;; (hopefully) get us onto the critical surface in the k0-k3 phase diagram
 
 ;; alpha is set to -1 for consistency. Other values are possible.
-;(set-k0-k3-alpha 1.0 0.75772 -1)
-(set-k0-k3-alpha 1.0 0.862 -1)
+(set-k0-k3-alpha 1.0 0.75772 -1)
+;(set-k0-k3-alpha 1.0 0.862 -1)
 
 ;; Take data!
 (generate-data-v2)
---
+++ 2p1-fixed-boundaries/tuning_utilities.lisp
@@ -0,0 +1,73 @@
+;;;; tuning_utilities.lisp
+;;;; Jonah Miller (jonah.maxwell.miller@gmail.com)
+
+;;;; This script is designed to be used with map_phase_space.py. It
+;;;; helps the simulation take data on volume variation.
+
+(defun run-thermalization-sweeps (sweeps)
+  "Does several sweeps without taking any data on them.
+   Allows for the system volume to thermalize, which takes time."
+  (for (i 0 sweeps) 
+    (sweep)))
+
+(defun collect-volume-data (sweeps)
+  "Similar to the collect data functions in montecarlo.lisp, 
+but designed to test the phase space."
+  (let ((3-volumes nil))
+    (for (ns 0 sweeps)
+	 (sweep)
+	 (push (+ N3-TL-31 N3-TL-22) 3-volumes))
+    3-volumes))
+
+(defun mean (list-of-values)
+  "Calculate the mean of a list of values"
+  (float (/ (reduce #'+ list-of-values) (length list-of-values))))
+
+(defun standard-deviation (list-of-values)
+  "Calculate the standard deviation of a list of values"
+  (flet ((sqr (x) (* x x)))
+    (let* ((n (length list-of-values))
+	   (mean (mean list-of-values))
+	   (deviation-list (mapcar #'(lambda (x) (sqr (- x mean))) 
+				   list-of-values)))
+      (sqrt (/ (reduce #'+ deviation-list) (1- n))))))
+
+(defun get-delta (list-of-values)
+  "Calculate a crude derivative for a
+ list of values (as a function of list index."
+  (* -1 (- (nth (1- (length list-of-values)) list-of-values) 
+	   (first list-of-values))))
+
+(defun test-volume-variation (sweeps &optional (k-and-litl nil))
+  "Runs collect-volume-data and then does statistics on it, and prints it to 
+  the terminal."
+  (let* ((volumes (collect-volume-data sweeps))
+	 (avg (mean volumes))
+	 (std (standard-deviation volumes))
+	 (deltav (get-delta volumes))
+	 (datalist (list avg std deltav)))
+    (if k-and-litl 
+	(format t "~A ~A ~A ~A ~A~%" *k* *litl* avg std deltav)
+	(format t "~A ~A ~A ~A ~A~%" *k0* *k3* avg std deltav))
+    datalist))
+	 
+(defun add-data-to-file (outfile n3-mean n3-sigma n3-delta 
+			 &optional (k-and-litl nil))
+  "Adds the requisite information to the output file."
+  (with-open-file (f outfile
+		     :direction :output
+		     :if-exists :append
+		     :if-does-not-exist :create)
+    (if k-and-litl
+	(format f "~3D~T~3D~T~3D~T~3D~T~3D~%" *k* *litl* n3-mean n3-sigma 
+		n3-delta)
+	(format f "~3D~T~3D~T~3D~T~3D~T~3D~%" *k0* *k3* n3-mean n3-sigma 
+		n3-delta))))
+
+(defun take-data-and-print-to-file (outfile sweeps)
+  "Take volume data and then print it to the given file."
+    (let* ((volume-data (test-volume-variation sweeps))
+	   (avg (nth 0 volume-data))
+	   (std (nth 1 volume-data))
+	   (deltav (nth 2 volume-data)))
+      (add-data-to-file outfile avg std deltav)))
