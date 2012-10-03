637e975a348b3eb9a8c3bf5a4ec7924e896d0af0
Yurlungur <DrLoco3000@gmail.com>
2012-10-03 15:48:49 -0600

    new file:   sphere_generator/documentation/README 	new file:   sphere_generator/documentation/sphere_generator.pdf 	new file:   sphere_generator/documentation/sphere_generator.tex

---
+++ sphere_generator/documentation/auto/_region_.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook "_region_"
+ (lambda ()
+    (TeX-run-style-hooks
+     "latex2e"
+     "art10"
+     "article")))
+
---
+++ sphere_generator/documentation/auto/sphere_generator.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook "sphere_generator"
+ (lambda ()
+    (TeX-run-style-hooks
+     "latex2e"
+     "art10"
+     "article")))
+
