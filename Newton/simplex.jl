85882602997a5e10786abf121bfccd0bc4d9979d
Adam Getchell <acgetchell@ucdavis.edu>
2013-06-02 01:56:18 -0700

    Skeleton simplex and simplex doc files

---
+++ Newton/simplex.jl
@@ -0,0 +1,15 @@
+# 4-simplices and sub-simplices
+#
+# A [Simplex] is a generalized d-dimensional equilateral triangle.
+# TODO: Import CGAL
+
+require("utilities.jl")
+
+
+
+
+
+
+# ## References
+#
+# [Simplex]: http://en.wikipedia.org/wiki/Simplex
