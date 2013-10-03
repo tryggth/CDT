0b6e2c77cdcc108ad33f2ac123e0d807ed9807be
Jonah Miller <DrLoco3000@gmail.com>
2013-10-03 17:03:08 -0400

    modified:   data_analysis_scripts/get_subspacetime.py

--- 2p1-fixed-boundaries/data_analysis_scripts/get_subspacetime.py
+++ 2p1-fixed-boundaries/data_analysis_scripts/get_subspacetime.py
@@ -2,7 +2,7 @@
 
 # get_subspacetime.py
 # Author: Jonah Miller (jonah.maxwell.miller@gmail.com)
-# Time-stamp: <2013-09-07 22:40:03 (jonah)>
+# Time-stamp: <2013-10-03 16:52:14 (jonah)>
 
 # This program loads a spacetime file and generates a new file based
 # on the old one. The new file is for a spacetime with initial
@@ -48,7 +48,55 @@ def new_file_name(old_file_name,initial_slice,final_slice):
                                                           final_slice)
     output=old_file_name[:-6]+subspacetime_designation+old_file_name[-6:]
     return output
-    
+
+def make_output_string(output_list):
+    """
+    takes a simplex list of the form
+
+    [type, tmlo, tmhi, point1, point2, point3, point4, neighbor1,
+     neighbor2, neighbor3, neighbor4, simplexID]
+
+    and outputs it as a space-separated list that's a string.
+    """
+    return reduce(lambda x,y: "{} {}".format(x,y),output_list) + "\n"
+
+def in_bulk(simplex,initial_slice,final_slice):
+    """
+    takes a simplex list of the form
+
+    [type, tmlo, tmhi, point1, point2, point3, point4, neighbor1,
+     neighbor2, neighbor3, neighbor4, simplexID]
+
+    And checks whether or not initial_slice < tmlo and final_slice > tmhi.
+
+    This excludes simplices on the boundary of the new subspacetime.    
+    """
+    tmlo = simplex[1]
+    tmhi = simplex[2]
+    return tmlo > initial_slice and tmhi < final_slice
+
+def on_boundary(simplex, initial_slice,final_slice):
+    """
+    takes a simplex list of the form
+
+    [type, tmlo, tmhi, point1, point2, point3, point4, neighbor1,
+     neighbor2, neighbor3, neighbor4, simplexID]
+
+    And checks whether or not the simplex is on the boundary of the
+    subspacetime.
+    """
+    tmlo = simplex[1]
+    tmhi = simplex[2]
+    return tmlo == initial_slice or tmhi == final_slice
+
+def make_new_first_line(first_line,initial_slice, final_slice):
+    """
+    Takes the string that's the first line of the file that has
+    parameter information and resets the number of time slices.
+    """
+    line_list = first_line.split(' ')
+    line_list[2] = str(final_slice - initial_slice)
+    return reduce(lambda x,y: "{} {}".format(x,y),line_list)    
 
 def extract_subspacetime(old_file_name,initial_slice,final_slice):
     """
@@ -63,7 +111,10 @@ def extract_subspacetime(old_file_name,initial_slice,final_slice):
 
             # Copy the first line if the infile to the outfile. This
             # is the list of simulation parameters.
-            outfile.write(infile.readline())
+            first_line = make_new_first_line(infile.readline(),
+                                            initial_slice,
+                                            final_slice)
+            outfile.write(first_line)
 
             # Now iterate through the file and read each remaining
             # line. Each line represents a 3-simplex. It's a space
@@ -76,12 +127,43 @@ def extract_subspacetime(old_file_name,initial_slice,final_slice):
             # We copy the line from the old file to the new file if
             # and only if tmlo is greater than initial_slice and tmhi
             # is less than final_slice.
-            for line in infile:
-                simplex=line.split()
-                tmlo=int(simplex[1])
-                tmhi=int(simplex[2])
-                if tmlo > initial_slice and tmhi < final_slice:
-                    outfile.write(line)
+            #
+            # Unfortunately, we need to copy the spacetime
+            # carefully. If a simplex has a "neighbor" that's outside
+            # the boundary time slices, we need to remove this
+            # neighbor. Neighbors "zero" are considered void pointers
+            # and are ignored. So we need to ensure that neighbors are
+            # treated this way.
+
+            # First we read in the file and store each line
+            lines = infile.readlines()
+            # Then we make a hash table that maps a simplex id to the
+            # apppropriate simplex and the string in associated with
+            # it in the file
+            simplices = {}
+            for line in lines:
+                simplex = [int(i) for i in line.split()]
+                simplices[simplex[-1]] = simplex
+
+            # Now, we go back through each line and, if the simplex is
+            # in the bulk, print it to the new outfile. If the simplex
+            # is on the boundary, ensure all its neighbors are on the
+            # boundary too. Then print it to the file.
+            for simplex_id in simplices.keys():
+                simplex = simplices[simplex_id]
+                if in_bulk(simplex,initial_slice,final_slice):
+                    outfile.write(make_output_string(simplex))
+                if on_boundary(simplex,initial_slice,final_slice):
+                    for i in range(7,11):
+                        if simplex[i] != 0:
+                            neighbor = simplices[simplex[i]]
+                            if not (in_bulk(neighbor,initial_slice,
+                                            final_slice)\
+                                        or on_boundary(neighbor,
+                                                       initial_slice,
+                                                       final_slice)):
+                                simplex[i] = 0
+                    outfile.write(make_output_string(simplex))
     return outfile_path
 
 def main(system_arguments):
